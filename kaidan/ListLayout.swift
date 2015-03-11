849d98b97d423cab24157ead7fb685e53321e776
mizota <t.mizota@conol.jp>
2015-03-11 16:15:48 +0900

    コレクションの一覧レイアウトを決定するクラスの作成（適用はこれから）

---
+++ kaidan/ListLayout.swift
@@ -0,0 +1,18 @@
+//
+//  ListLayout.swift
+//  kaidan
+//
+//  Created by 溝田隆明 on 2015/03/11.
+//  Copyright (c) 2015年 tebcro. All rights reserved.
+//
+
+import UIKit
+
+class ListLayout: UICollectionViewLayout {
+   
+    //一覧の見え方を決定するレイアウトファイル
+    override func prepareLayout()
+    {
+        
+    }
+}
