b4bd9d6d2f90ce17a4a0139b4cd167c446152ff1
mizota <t.mizota@conol.jp>
2015-03-12 17:21:04 +0900

    詳細用のレイアウトファイルを作成

---
+++ kaidan/DetailLayout.swift
@@ -0,0 +1,17 @@
+//
+//  DetailLayout.swift
+//  kaidan
+//
+//  Created by 溝田隆明 on 2015/03/12.
+//  Copyright (c) 2015年 tebcro. All rights reserved.
+//
+
+import UIKit
+
+class DetailLayout: UICollectionViewLayout {
+    
+    override func prepareLayout()
+    {
+        
+    }
+}
